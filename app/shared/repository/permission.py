937eb8b88cd2cb299e2615c05122c016e1444366
hello <hello@email.com>
2026-03-04 10:19:32 +0800

    update permission table

--- app/shared/repository/permission.py
+++ app/shared/repository/permission.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, String, UniqueConstraint
+from sqlalchemy import Boolean, DateTime, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core import db
@@ -22,7 +22,14 @@ class Permission(db.Model):
     module: Mapped[str] = mapped_column(String(50), nullable=False)
     action: Mapped[str] = mapped_column(String(30), nullable=False)
     description: Mapped[str | None] = mapped_column(String(100))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=now_utc0)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=now_utc0,
+        onupdate=now_utc0,
+    )
 
     users: Mapped[list["UserPermission"]] = relationship(
         back_populates='permission',
