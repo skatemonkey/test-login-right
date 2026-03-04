f1cc3c3d37ba158b30d2285a352cded9ebece3e0
hello <hello@email.com>
2026-03-04 10:19:32 +0800

    feature-lok change token creation to just put user.id in int

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -16,7 +16,7 @@ def login(req: LoginRequest):
     ]
 
     access_token = create_access_token(
-        identity=str(user.user_id),
+        identity=user.user_id,
         additional_claims={"username": user.username}
     )
     return LoginResponse(user_id=user.user_id, username=user.username, access_token=access_token,
